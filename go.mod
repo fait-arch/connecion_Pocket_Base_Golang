29776bc4ccd3c08e6d21f6ba2864f44ef5c8708c
Juan <70347526+fait-arch@users.noreply.github.com>
2023-11-28 14:31:49 -0500

    Levantamiento de Arbol de Archivos
    Se creo el proyecto

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fait-arch/connecion_Pocket_Base_Golang
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,11 @@
+/*Server con golang*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Slect")
+}
