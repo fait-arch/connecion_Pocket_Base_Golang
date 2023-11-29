f3f65d80651287c2d2e34b12c1a63c282ee7d9f6
Juan <70347526+fait-arch@users.noreply.github.com>
2023-11-28 20:04:10 -0500

    Coneccion "8080"
    Conecto el puerto 8080 con GIN

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 /*Server con golang*/
-
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Slect")
+	router := gin.Default()
+
+	// Rutas estáticas para servir archivos estáticos
+	router.Static("/static", "./public_web/static")
+	router.Static("/img", "./public_web/templates/img")
+	router.LoadHTMLGlob("public_web/templates/*.html")
+
+	// Ruta para la página principal
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	// Habilitar el puerto 8080
+	router.Run(":8080")
 }
